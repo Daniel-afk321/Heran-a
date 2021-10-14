fa200863d5d543f9bfd18f50d39be3f03263b42f
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2021-10-13 21:46:10 -0300

    comentario

--- Herança/Program.cs
+++ Herança/Program.cs
@@ -8,7 +8,7 @@ namespace Herança
 
         static void Main(string[] args)
         {
-            // esta mostrando tudo da conta juridica e a que esta associada tambem que é a conta 
+            
             ContaJuridica Conta = new ContaJuridica(8010,"LaraCroft", 100.0, 500.0); 
             //esta mostrando o saldo
             Console.WriteLine(Conta.Saldo);
